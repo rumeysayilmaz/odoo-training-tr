1cc1b9c1f9cde96ef9c50186b9ffa0311bf9c1f0
rumeysayilmaz <era.ylmaz@gmail.com>
2019-09-02 17:12:41 +0300

    Add new files

---
+++ certification/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name':'Certification',
+    'summary':'Defines certificatiob for different purposes',
+    'author':'Odoo training class',
+    'website':'https://github.com/rumeysayilmaz/odoo-training-tr',
+    'category':'Certification Management',
+    'version': '12.0.1.0.0',
+    'license':'AGPL-3',
+    'depends':['base'],
+    'data':[
+        'security/ir.model.access.csv',
+        'views/certification_standard.xml',
+        'views/certification_view.xml',
+        'views/res_partner.xml'],
+    'development_status':'Beta',
+    'maintainers': ['eficient'],
+}
