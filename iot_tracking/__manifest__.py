e619af8d4dd90d1b182de680addaeffd8eb74360
rumeysayilmaz <era.ylmaz@gmail.com>
2019-09-04 16:59:26 +0300

    created	new file:   iot_tracking/__init__.py created	new file:   iot_tracking/__manifest__.py created	new file:   iot_tracking/models/__init__.py created	new file:   iot_tracking/models/__pycache__/__init__.cpython-36.pyc created	new file:   iot_tracking/models/__pycache__/authorization.cpython-36.pyc created	new file:   iot_tracking/models/__pycache__/device.cpython-36.pyc created	new file:   iot_tracking/models/__pycache__/device_object.cpython-36.pyc created	new file:   iot_tracking/models/__pycache__/location.cpython-36.pyc created	new file:   iot_tracking/models/__pycache__/movement.cpython-36.pyc created	new file:   iot_tracking/models/authorization.py created	new file:   iot_tracking/models/device.py created	new file:   iot_tracking/models/device_object.py created	new file:   iot_tracking/models/location.py created	new file:   iot_tracking/models/movement.py 	deleted:    tracking_app/.___init__.py 	deleted:    tracking_app/.___manifest__.py 	deleted:    tracking_app/._models 	deleted:    tracking_app/._security 	deleted:    tracking_app/._static 	deleted:    tracking_app/._views 	deleted:    tracking_app/__init__.py

---
+++ iot_tracking/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "iot Device Tracking Application",
+
+    'summary': """
+        This module tracks the movements of device objects assigned to company resources such as employees, machines and provides reporting facilities for end users""",
+    'author':'ABdulkadir Yilmaz, EMine Rumeysa Yilmaz',
+    'website':'https://github.com/rumeysayilmaz/odoo-training-tr',
+    'license':'AGPL-3',
+    'depends':['base'],
+    'data':[
+        ],
+    'development_status':'Beta',
+    'maintainers': ['rumeysa yilmaz abdulkadir yilmaz'],
+}
---
+++ iot_tracking/models/__init__.py
@@ -0,0 +1,5 @@
+from . import device
+from . import device_object
+from . import location
+from . import movement
+from . import authorization
